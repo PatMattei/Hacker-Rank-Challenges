280a9fa928675db1e2ea238fcfbdde25aaf01700
Patrick Mattei <patrick.mattei@gmail.com>
2020-11-06 17:32:04 -0500

    WIP: new year chaos- must add bribes count correctly

---
+++ new_year_chaos.js
@@ -0,0 +1,49 @@
+// Complete the minimumBribes function below.
+function minimumBribes(q) {
+	let bribes = 0;
+	//a is the original line; q is the line after cuts and bribes have been made
+	let a = new Array(q.length); // create an empty array with length 45
+	for(var i = 0; i < a.length; i++){
+		a[i] = i + 1; 
+	}
+	
+	for(var i = 0; i < q.length; i++){
+		if ((q[i] - a[i]) > 2) { //too chaotic if there were more than 2 jumps
+			return "Too chaotic";
+		} 
+	}
+
+	while (a[a.length-1] !== q[a.length-1]) {
+		for(var i = 0; i < q.length; i++){
+			if (q[i] != a[i]) {
+				let indx = a.indexOf(q[i]);
+				let num = q[i];
+
+				console.log(q[i])
+
+				bribes+= (indx-i);
+
+				a.splice(i, 0, num);
+				a.splice(indx, 1);
+			}
+		}
+	}
+	return bribes;
+}
+
+console.log(minimumBribes([ 1, 2, 5, 3, 7, 8, 6, 4 ]));
+
+
+// 1 2 3 4 5 6 7 8
+// 1 2 3 4 5 7 6 8    7
+// 1 2 3 4 5 7 8 6    8
+// 1 2 3 5 4 7 8 6    5
+// 1 2 5 3 4 7 8 6    5
+// 1 2 5 3 7 4 8 6    7
+// 1 2 5 3 7 8 4 6    8
+// 1 2 5 3 7 8 6    6
+
+// 1 2 5 3 7 8 6 4
+
+
+// 7
