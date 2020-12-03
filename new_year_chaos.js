cbf46cf5e8be9a52ae72c49e6231ef351b21b081
Patrick Mattei <patrick.mattei@gmail.com>
2020-12-03 11:23:48 -0500

    condensed into single for loop

--- new_year_chaos.js
+++ new_year_chaos.js
@@ -3,13 +3,10 @@ function minimumBribes(q) {
 	let bribes = 0;
 	
 	for(var i = 0; i < q.length; i++){
-		if ((q[i] - (i+1)) > 2) { //too chaotic if there were more than 2 jumps
+		if ((q[i] - (i + 1)) > 2) { //too chaotic if there were more than 2 jumps
 			return "Too chaotic";
-		} 
-	}
+		}
 
-	//for each number in q:
-	for(var i = 0; i < q.length; i++) {
 		//if a person was bribed
 		if (q[i] < i + 1) {
 			//iterate through q up to i
@@ -24,12 +21,4 @@ function minimumBribes(q) {
 	return bribes;
 }
 
-console.log(minimumBribes([ 1, 2, 5, 3, 7, 8, 6, 4 ]));
-
-//q 1 2 5 3 7 8 6 4
-//a 1 2 3 4 5 6 7 8
-
-//3 was bribed 1
-//4 was bribed 4
-//6 was bribed 2
-// 7
+console.log(minimumBribes([ 2, 1, 5, 3, 4 ]));
