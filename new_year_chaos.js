c04b4c9ba762eb33d2a7ae298fda6cd1a7f414d5
Patrick Mattei <patrick.mattei@gmail.com>
2020-12-04 09:50:14 -0500

    Updated test case

--- new_year_chaos.js
+++ new_year_chaos.js
@@ -1,25 +1,25 @@
 // Complete the minimumBribes function below.
 function minimumBribes(q) {
-	let bribes = 0;
-	
-	for(let i = 0; i < q.length; i++){
-		if ((q[i] - (i + 1)) > 2) { //too chaotic if there were more than 2 jumps
-			return "Too chaotic";
-		}
+    let bribes = 0;
+    
+    for(let i = 0; i < q.length; i++){
+        if ((q[i] - (i + 1)) > 2) { //too chaotic if there were more than 2 jumps
+            return "Too chaotic";
+            return;
+        }
 
-		//if a person was bribed
-		if (q[i] < i + 1) {
-			//iterate through q up to i
-			for(let j = 0; j < i; j++) {
-				if (q[j] > q[i]) {
-					bribes++;
-					console.log(q[i], " was bribed by ", q[j], "now we have ", bribes, " bribes.")
-				}
-			}
-		}
-	}
+        //if a person was bribed
+        else if (q[i] < i + 1) {
+            //iterate through q up to i
+            for(let j = 0; j < i; j++) {
+                if (q[j] > q[i]) {
+                    bribes++;
+                }
+            }
+        }
+    }
 
-	return bribes;
+    return bribes;
 }
 
 console.log(minimumBribes([ 1, 2, 5, 3, 7, 8, 6, 4]));
